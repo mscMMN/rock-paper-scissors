fc68d869e73bb288505e1bcffcda0510289b4f3c
mscMMN <github@markus-nakhlah.de>
2020-11-28 10:27:13 +0100

    Improve tests by using mocking PlayerStrategy

--- src/test/kotlin/de/nakhlah/markus/player/PlayerTest.kt
+++ src/test/kotlin/de/nakhlah/markus/player/PlayerTest.kt
@@ -1,22 +1,40 @@
 package de.nakhlah.markus.player
 
 import de.nakhlah.markus.game.Action
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(MockKExtension::class)
 internal class PlayerTest {
+
+    @MockK
+    private lateinit var playerStrategy: PlayerStrategy
 
     private lateinit var playerSUT: Player
 
     @BeforeEach
     internal fun setUp() {
-        playerSUT = Player("Markus", PlayerStrategyFixed(Action.PAPER))
+        every { playerStrategy.showAction() } returns Action.PAPER
+
+        playerSUT = Player("Markus", playerStrategy)
     }
 
     @Test
     internal fun showAction() {
         assertEquals("Markus", playerSUT.name)
         assertEquals(Action.PAPER, playerSUT.showAction())
+
+        verify { playerStrategy.showAction() }
+    }
+
+    @Test
+    internal fun shouldUseTheNameForToString() {
+        assertEquals("Markus", "$playerSUT")
     }
 }

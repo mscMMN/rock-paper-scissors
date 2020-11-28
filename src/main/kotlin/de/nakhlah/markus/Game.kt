19bc1fd37eb6fb48b72c6fa42629d9de319c378c
mscMMN <github@markus-nakhlah.de>
2020-11-28 09:37:42 +0100

    Use rounds to play the game multiple times and collect stats

--- src/main/kotlin/de/nakhlah/markus/Game.kt
+++ src/main/kotlin/de/nakhlah/markus/Game.kt
@@ -2,6 +2,23 @@ package de.nakhlah.markus
 
 import de.nakhlah.markus.player.Player
 
-class Game(firstPlayer: Player, secondPlayer: Player, numberOfRounds: Int) {
+class Game(private val firstPlayer: Player, private val secondPlayer: Player, private val numberOfRounds: Int) {
 
+    private val rounds = (1..numberOfRounds).map { Round(it) }
+
+    fun play() {
+        println("The game begins")
+
+        rounds.forEach { it.play(firstPlayer, secondPlayer) }
+
+        println("The game is over")
+
+        val winsForFirstPlayer = rounds.count { firstPlayer == it.winner }
+        val winsForSecondPlayer = rounds.count { secondPlayer == it.winner }
+        val draws = rounds.count { it.isDraw }
+
+        println("${firstPlayer.name} has won $winsForFirstPlayer times")
+        println("${secondPlayer.name} has won $winsForSecondPlayer times")
+        println("Out of ${rounds.size} rounds there have been $draws draws")
+    }
 }

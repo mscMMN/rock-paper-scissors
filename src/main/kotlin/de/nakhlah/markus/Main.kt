96b26b131ee320ff9a05ef450bd87d4d1cba65b3
mscMMN <github@markus-nakhlah.de>
2020-11-28 09:39:57 +0100

    Play the game with 100 rounds and two different player strategies

--- src/main/kotlin/de/nakhlah/markus/Main.kt
+++ src/main/kotlin/de/nakhlah/markus/Main.kt
@@ -1,6 +1,15 @@
 package de.nakhlah.markus
 
+import de.nakhlah.markus.player.Player
+import de.nakhlah.markus.player.PlayerStrategyFixed
+import de.nakhlah.markus.player.PlayerStrategyRandom
+
 fun main(args: Array<String>) {
     println("Welcome to Rock, Paper, Scissors!")
+
+    val firstPlayer = Player("Markus", PlayerStrategyRandom())
+    val secondPlayer = Player("The Rock™", PlayerStrategyFixed(Action.ROCK))
+
+    Game(firstPlayer, secondPlayer, 100).play()
 }
 

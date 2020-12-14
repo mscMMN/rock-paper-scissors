b74f0101e78ea616dfa2020f00e0581a1a578eb8
mscMMN <github@markus-nakhlah.de>
2020-12-14 15:37:53 +0100

    Extend game with Spock and Lizard actions

--- src/main/kotlin/de/nakhlah/markus/game/Action.kt
+++ src/main/kotlin/de/nakhlah/markus/game/Action.kt
@@ -3,11 +3,15 @@ package de.nakhlah.markus.game
 enum class Action {
     ROCK,
     PAPER,
-    SCISSORS
+    SCISSORS,
+    SPOCK,
+    LIZARD
 }
 
 fun Action.beats(otherAction: Action): Boolean = when (this) {
-    Action.PAPER -> Action.ROCK == otherAction
-    Action.ROCK -> Action.SCISSORS == otherAction
-    Action.SCISSORS -> Action.PAPER == otherAction
+    Action.PAPER -> otherAction in setOf(Action.ROCK, Action.SPOCK)
+    Action.ROCK -> otherAction in setOf(Action.SCISSORS, Action.LIZARD)
+    Action.SCISSORS -> otherAction in setOf(Action.PAPER, Action.LIZARD)
+    Action.LIZARD -> otherAction in setOf(Action.PAPER, Action.SPOCK)
+    Action.SPOCK -> otherAction in setOf(Action.ROCK, Action.SCISSORS)
 }
--- src/test/kotlin/de/nakhlah/markus/game/ActionTest.kt
+++ src/test/kotlin/de/nakhlah/markus/game/ActionTest.kt
@@ -6,17 +6,32 @@ import org.junit.jupiter.api.Test
 internal class ActionTest {
 
     @Test
-    fun paperBeatsRock() {
+    fun `Paper beats Spock and Rock`() {
+        assertTrue(Action.PAPER.beats(Action.SPOCK))
         assertTrue(Action.PAPER.beats(Action.ROCK))
     }
 
     @Test
-    fun rockBeatsScissors() {
+    fun `Rock beats Lizard and Scissors`() {
+        assertTrue(Action.ROCK.beats(Action.LIZARD))
         assertTrue(Action.ROCK.beats(Action.SCISSORS))
     }
 
     @Test
-    fun scissorsBeatsPaper() {
+    fun `Lizard beats Paper and Spock`() {
+        assertTrue(Action.LIZARD.beats(Action.PAPER))
+        assertTrue(Action.LIZARD.beats(Action.SPOCK))
+    }
+
+    @Test
+    fun `Spock beats Scissors and Rock`() {
+        assertTrue(Action.SPOCK.beats(Action.SCISSORS))
+        assertTrue(Action.SPOCK.beats(Action.ROCK))
+    }
+
+    @Test
+    fun `Scissors beats Paper and Lizard`() {
         assertTrue(Action.SCISSORS.beats(Action.PAPER))
+        assertTrue(Action.SCISSORS.beats(Action.LIZARD))
     }
 }
